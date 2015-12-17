0b6cb991989a092964cc3e820ec74c500c0401b5
Lopez Ezequiel <lopezezequiel.09@gmail.com>
2015-12-17 00:01:26 -0300

    Added new captcha

--- test.py
+++ test.py
@@ -10,5 +10,10 @@ captcha = Image.open('02.png')
 fn = lambda color: 0 if color == (255,255,255) else 1
 print solve_captcha(captcha, fn, 'patterns02')
 
+#Solving captcha3
+captcha = Image.open('03.png')
+fn = lambda color: 0 if color not in [(230,200,230), (245,245,245)]  else 1
+print solve_captcha(captcha, fn, 'patterns03')
+
 #Generating patterns
-generate_patterns('samples02', 'patterns03', fn)
+generate_patterns('samples03', 'patterns04', fn)
